8d3e94c5ff40234aac86591f45cb074a4761ba10
kieroid <40911271+kieroid@users.noreply.github.com>
2023-12-31 17:29:05 +0100

    update all these things

--- README.md
+++ README.md
@@ -3,7 +3,17 @@ suckless dmenu - simple fork
 simple fork of dmenu with the patches that i used pre-applied.<br/>
 dmenu is an efficient dynamic menu for X.
 
-Installation
+patches
+------------
+dmenu-border<br/>
+dmenu-caseinsensitive<br/>
+dmenu-center<br/>
+dmenu-date<br/>
+dmenu-listfullwidth<br/>
+dmenu-mousesupport<br/>
+dmenu-numbers<br/>
+
+installation
 ------------
 edit config.mk to match your system<br/>
 type `sudo make install` to build dmenu, then install.
--- config.def.h
+++ config.def.h
@@ -1,28 +1,24 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
-static int colorprompt = 1;    
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* kieran fork thing that i stole from suckless */
+
+static int colorprompt = 1; /* i forgot what this does, but im keeping it on */    
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1; /* -c option; centers dmenu on screen */
+static int min_width = 500; /* minimum width when centered */
+
+/* colors, fonts, and words */
+static const char *fonts[] = {"monospace:size=10"};
+static const char *prompt      = "apps";
+static const char col_a[] = "#ffffff";
+static const char col_b[] = "#000000";
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/* typ          fg     bg     */
+	[SchemeNorm] ={ col_a, col_b },
+	[SchemeSel] = { col_b, col_a },
+	[SchemeOut] = { col_b, col_a },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
+static unsigned int lines      = 12;
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
 static unsigned int border_width = 1;
