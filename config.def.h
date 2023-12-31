bdeae71663bab1d75ca568e95d8e9e4035d07d59
kieroid <40911271+kieroid@users.noreply.github.com>
2023-12-31 21:53:42 +0100

    add opacity to dmenu

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static int colorprompt = 1; /* i forgot what this does, but im keeping it on */
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1; /* -c option; centers dmenu on screen */
 static int min_width = 500; /* minimum width when centered */
+static const unsigned int alpha = 0x88;     /* Amount of opacity. 0xff is opaque */
 
 /* colors, fonts, and words */
 static const char *fonts[] = {"monospace:size=10"};
@@ -17,6 +18,12 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { col_b, col_a },
 	[SchemeOut] = { col_b, col_a },
 };
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 12;
