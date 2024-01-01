9cbb4e73daf2f1a51dc16bca56e14906ea0af642
kieroid <40911271+kieroid@users.noreply.github.com>
2024-01-01 20:13:44 +0100

    update config

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 /* kieran fork thing that i stole from suckless */
 
-static int colorprompt = 1; /* i forgot what this does, but im keeping it on */    
-static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1; /* -c option; centers dmenu on screen */
-static int min_width = 500; /* minimum width when centered */
-static const unsigned int alpha = 0xBB;     /* Amount of opacity. 0xff is opaque */
+static int colorprompt 			= 1; /* i forgot what this does, but im keeping it on */    
+static int topbar 			= 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int centered 			= 1; /* -c option; centers dmenu on screen */
+static int min_width 			= 500; /* minimum width when centered */
+static const unsigned int alpha 	= 0xBB;     /* Amount of opacity. 0xff is opaque */
 
 /* colors, fonts, and words */
-static const char *fonts[] = {"monospace:size=10"};
-static const char *prompt      = "apps";
-static const char col_a[] = "#ffffff";
-static const char col_b[] = "#000000";
+static const char *fonts[] 		= {"monospace:size=10"};
+static const char *prompt      		= "apps:";
+static const char col_a[] 		= "#ffffff";
+static const char col_b[] 		= "#000000";
 static const char *colors[SchemeLast][2] = {
 	/* typ          fg     bg     */
 	[SchemeNorm] ={ col_a, col_b },
@@ -26,6 +26,6 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 12;
-static const char worddelimiters[] = " ";
-static unsigned int border_width = 1;
+static unsigned int lines      		= 12;
+static const char worddelimiters[] 	= " ";
+static unsigned int border_width 	= 1;
